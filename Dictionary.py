35c4c3208e1de117321bd8d49ee59924af6159a4
Landon <94721040+RedSpikeLandon@users.noreply.github.com>
2023-08-28 16:14:37 -0400

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+d = {
+    "Clock": "Dock",
+    "Fought": "Sock",
+    "Walk": "Lot",
+    "Palindrome": "Chromosome",
+    "Air": "Water"
+}
+print (d["Clock"])
+
+
---
+++ List.py
@@ -0,0 +1,2 @@
+new_list= ["Landon", "Jaymin", "Book", "Mat", "Table", "Drawer", "Clothes", "Closet"]
+print(new_list)
