168b8adc1ba60f9838a6992975701b815b5eba15
RedSpikeLandon <94721040+RedSpikeLandon@users.noreply.github.com>
2023-08-30 15:40:35 -0400

    anything

--- Class for Computer.py
+++
@@ -1,8 +0,0 @@
-class Computer:
-    def _init_(self, model, color, screen_brightness, connection, keyboard_language, size, chip):
-        self.keyboard = keyboard
-        self.size = size
-        self. 
-
-
-Computer("Macbook Pro, "Space Gray",  "14.0)"
--- List.py
+++
@@ -1,2 +0,0 @@
-new_list= ["Landon", "Jaymin", "Book", "Mat", "Table", "Drawer", "Clothes", "Closet"]
-print(new_list)
